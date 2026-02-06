c1ac181839f07d87c527c487995f9b6698a7093a
chrisnew143-gif <chrisnew143@gmail.com>
2026-02-06 13:24:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title("🎾 Pickleball Stack System")
 col1, col2 = st.columns(2)
 
 if col1.button("🏟 Organizer (AutoStack)", use_container_width=True):
-    st.switch_page("pages/1_AutoStack.py")
+    st.switch_page("pickleball-stack/1_AutoStack.py")
 
 if col2.button("👤 Player Join", use_container_width=True):
     st.switch_page("pages/2_PlayerJoin.py")
